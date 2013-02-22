e8a0fa59b2811491c9be5b6bb418e9bfb1945c77
mluto <m@luto.at>
2013-02-22 22:19:35 +0100

    Create dirs before using them in setup.sh, moved sharejs webclient to the proper directory

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 
 npm install .
 
+mkdir static/js
+
 cp node_modules/share/node_modules/browserchannel/dist/bcsocket.js static/js/bcsocket.js
-cp -r node_modules/share/webclient/ static/webclient/
+cp -r node_modules/share/webclient/ static/js/webclient/
 
 cd static/js
 wget "https://raw.github.com/mattsnider/jquery-plugin-query-parser/master/jquery-queryParser.min.js" -O jquery-queryParser.min.js
