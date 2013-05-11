b0889b2505d72029bb89230ca695012126e115c3
mluto <m@luto.at>
2013-05-11 17:18:09 +0200

    Added PWP to asyncnoter

--- setup.sh
+++ setup.sh
@@ -9,8 +9,12 @@ cd js
 wget "https://raw.github.com/mattsnider/jquery-plugin-query-parser/master/jquery-queryParser.min.js" -O jquery-queryParser.min.js
 wget "http://www.appelsiini.net/download/jquery.jeditable.mini.js" -O jquery.jeditable.mini.js
 wget "https://raw.github.com/ether/etherpad-lite-jquery-plugin/master/js/etherpad.js" -O etherpad.js
+wget "https://raw.github.com/podlove/podlove-web-player/master/podlove-web-player/podlove-web-player.js" -O podlove-web-player.js
+wget "https://raw.github.com/podlove/mediaelement/master/build/mediaelement-and-player.min.js" -O mediaelement-and-player.min.js
 cd ..
 
 mkdir css
 cd css
+wget "https://raw.github.com/podlove/podlove-web-player/master/podlove-web-player/podlove-web-player.css" -O podlove-web-player.css
+wget "https://raw.github.com/podlove/mediaelement/master/build/mediaelementplayer.min.css" -O mediaelementplayer.min.css
 cd ..
