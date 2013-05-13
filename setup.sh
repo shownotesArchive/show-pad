372941101712f2ed6843d75557ec513536b98f5d
mluto <m@luto.at>
2013-05-13 21:34:19 +0200

    Integrate the PWP properly (fixes icons)

--- setup.sh
+++ setup.sh
@@ -9,12 +9,15 @@ cd js
 wget "https://raw.github.com/mattsnider/jquery-plugin-query-parser/master/jquery-queryParser.min.js" -O jquery-queryParser.min.js
 wget "http://www.appelsiini.net/download/jquery.jeditable.mini.js" -O jquery.jeditable.mini.js
 wget "https://raw.github.com/ether/etherpad-lite-jquery-plugin/master/js/etherpad.js" -O etherpad.js
-wget "https://raw.github.com/podlove/podlove-web-player/master/podlove-web-player/podlove-web-player.js" -O podlove-web-player.js
-wget "https://raw.github.com/podlove/mediaelement/master/build/mediaelement-and-player.min.js" -O mediaelement-and-player.min.js
 cd ..
 
 mkdir css
 cd css
-wget "https://raw.github.com/podlove/podlove-web-player/master/podlove-web-player/podlove-web-player.css" -O podlove-web-player.css
-wget "https://raw.github.com/podlove/mediaelement/master/build/mediaelementplayer.min.css" -O mediaelementplayer.min.css
+cd ..
+
+mkdir pwp
+cd pwp
+wget https://github.com/podlove/podlove-web-player/archive/master.zip
+unzip master.zip
+rm master.zip
 cd ..
