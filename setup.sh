7f71ed92be69cbbc0b76947f3a9c73310d8f6ab5
mluto <m@luto.at>
2013-05-20 18:58:44 +0200

    asyncnoter: Proper classing for notes; scrolling for notes & scroll to current note

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ cd js
 wget "https://raw.github.com/mattsnider/jquery-plugin-query-parser/master/jquery-queryParser.min.js" -O jquery-queryParser.min.js
 wget "http://www.appelsiini.net/download/jquery.jeditable.mini.js" -O jquery.jeditable.mini.js
 wget "https://raw.github.com/ether/etherpad-lite-jquery-plugin/master/js/etherpad.js" -O etherpad.js
+wget "http://demos.flesler.com/jquery/scrollTo/js/jquery.scrollTo-min.js" -O jquery.scrollTo-min.js
 cd ..
 
 mkdir css
